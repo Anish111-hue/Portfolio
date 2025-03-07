5f0e2eb0f6f1433b3e28b4ae2bdf771d4a2ad9cd
Anish111-hue <koppulaanish111@gmail.com>
2025-03-07 11:20:45 -0500

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
-})
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+});
